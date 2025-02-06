dc12f9b4bc5adea57af2f4379b676f883de27b3f
dirk <dirk.p.reinhardt@ntnu.no>
2025-02-06 12:07:27 +0100

    Have pointmass example run parametric sensitivity test

--- tests/leap_c/test_point_mass.py
+++ tests/leap_c/test_point_mass.py
@@ -1,21 +1,35 @@
 import numpy as np
 from leap_c.examples.pointmass.mpc import PointMassMPC
+from leap_c.linear_mpc import LinearMPC
+from test_mpc_p_global import (
+    run_test_mpc_solve_and_batch_solve_on_batch_p_global,
+)
+from conftest import generate_batch_variation
+
+
+def test_parametric_sensitivities(
+    learnable_point_mass_mpc: LinearMPC, point_mass_mpc_p_global: np.ndarray
+):
+    run_test_mpc_solve_and_batch_solve_on_batch_p_global(
+        learnable_point_mass_mpc, point_mass_mpc_p_global, plot=False
+    )
 
 
 def main():
-    mpc = PointMassMPC(learnable_params=["m", "c", "q_diag"])
+    if False:
+        mpc = PointMassMPC(learnable_params=["m", "c", "q_diag"])
 
-    s = np.array([1.0, 0.0, 0.0, 0.0])
-    a = np.array([0.0, 0.0])
+        s = np.array([1.0, 0.0, 0.0, 0.0])
+        a = np.array([0.0, 0.0])
 
-    policy = mpc.policy(state=s, p_global=None)[0]
-    print(policy)
+        policy = mpc.policy(state=s, p_global=None)[0]
+        print(policy)
 
-    state_value = mpc.state_value(state=s, p_global=None)[0]
-    print(state_value)
+        state_value = mpc.state_value(state=s, p_global=None)[0]
+        print(state_value)
 
-    state_action_value = mpc.state_action_value(state=s, action=a, p_global=None)[0]
-    print(state_action_value)
+        state_action_value = mpc.state_action_value(state=s, action=a, p_global=None)[0]
+        print(state_action_value)
 
     # env = PointMassOcpEnv(mpc)
 
@@ -24,6 +38,18 @@ def main():
     # x0 = np.array([1.0, 1.0, 0.0, 0.0])
 
     # _ = env.step(mpc.policy(state=x0, p_global=None)[0])
+
+    n_batch = 4
+
+    learnable_point_mass_mpc = PointMassMPC(
+        learnable_params=["m", "c"], n_batch=n_batch
+    )
+
+    p_global = generate_batch_variation(
+        learnable_point_mass_mpc.ocp_solver.acados_ocp.p_global_values, n_batch
+    )
+
+    test_parametric_sensitivities(learnable_point_mass_mpc, p_global)
 
 
 if __name__ == "__main__":
