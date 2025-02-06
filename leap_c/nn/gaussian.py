fixed numerical bugs of std

leap_c/nn/gaussian.py
@@ -3,26 +3,24 @@
 from gymnasium import spaces
 import numpy as np
 import torch
-from torch.distributions import Normal, TransformedDistribution
-from torch.distributions.transforms import (
-    AffineTransform,
-    TanhTransform,
-)
 import torch.nn as nn
 import torch.nn.functional as F
 
 
 class Gaussian(nn.Module):
-    """A Gaussian transformed by a Gaussian.
+    """A Gaussian potentially transformed by a tanh function.
 
     The output is sampled from this distribution and then squashed with a tanh function.
     # TODO (Jasper): Why are we not using the transformed distr class from torch.
     """
 
+    scale: torch.Tensor
+    loc: torch.Tensor
+
     def __init__(
         self,
         action_space: spaces.Box,
-        log_std_min: float = -3.5,
+        log_std_min: float = -4,
         log_std_max: float = 2.0,
     ):
         """Initializes the TanhNormal module.
@@ -34,18 +32,15 @@ class Gaussian(nn.Module):
         self.log_std_min = log_std_min
         self.log_std_max = log_std_max
 
-        if not np.any(np.isinf(action_space.low)) and not np.any(
-            np.isinf(action_space.high)
-        ):
-            low = torch.tensor(action_space.low, dtype=torch.float32)
-            high = torch.tensor(action_space.high, dtype=torch.float32)
+        loc = (action_space.high + action_space.low) / 2.0
+        scale = (action_space.high - action_space.low) / 2.0
 
-            self.transform = [
-                AffineTransform(loc=low, scale=(high - low) / 2),
-                TanhTransform(),
-            ]
-        else:
-            self.transform = None
+        loc = torch.tensor(loc, dtype=torch.float32)
+        scale = torch.tensor(scale, dtype=torch.float32)
+
+
+        self.register_buffer("loc", loc)
+        self.register_buffer("scale", scale)
 
     def forward(
         self, mean: torch.Tensor, log_std: torch.Tensor, deterministic: bool = False
@@ -57,25 +52,23 @@ class Gaussian(nn.Module):
             deterministic: If True, the output will just be tanh(mean), no sampling is taking place.
 
         Returns:
-            an output sampled from the TanhNormal, the log probability of this output
+            An output sampled from the TanhNormal, the log probability of this output
             and a statistics dict containing the standard deviation.
         """
-        # TODO (Jasper): Do we need this?
-        std = torch.exp(log_std.clamp(self.log_std_min, self.log_std_max))
+        log_std = torch.clamp(log_std, self.log_std_min, self.log_std_max)
+        std = torch.exp(log_std)
 
-        distr = Normal(mean, std)
-
-        if self.transform is not None:
-            distr = TransformedDistribution(distr, self.transform)
+        normal = torch.distributions.Normal(mean, std)
 
         if deterministic:
             action = mean
-            if self.transform is not None:
-                for t in self.transform:
-                    action = t(action)
         else:
-            action = distr.rsample()
+            action = normal.rsample()
 
-        log_prob = distr.log_prob(action)
+        log_prob = normal.log_prob(action)
+        action = torch.tanh(action)
+        log_prob -= torch.log(self.scale * (1 - action.pow(2)) + 1e-6)
+
+        action = action * self.scale + self.loc
 
         return action, log_prob