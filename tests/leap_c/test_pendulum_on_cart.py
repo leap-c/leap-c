0a62978e2e6acb13f546ce0c2473ba88aef8de98
Confectio <leonard.fichtner@gmail.com>
2025-02-07 15:40:45 +0100

    Also recycle some tests that are more specific into the pendulum on cart tests

--- tests/leap_c/test_pendulum_on_cart.py
+++ tests/leap_c/test_pendulum_on_cart.py
@@ -7,7 +7,8 @@ import numpy as np
 import pytest
 from acados_template import AcadosOcpSolver
 from gymnasium.utils.save_video import save_video
-from leap_c.examples.pendulum_on_cart import PendulumOnCartMPC, PendulumOnCartOcpEnv
+from leap_c.examples.pendulum_on_a_cart.env import PendulumOnCartSwingupEnv
+from leap_c.examples.pendulum_on_cart import PendulumOnCartMPC
 from leap_c.util import create_dir_if_not_exists
 
 
@@ -66,11 +67,55 @@ def test_solution(
     fig, axs = plot_cart_pole_solution(ocp_solver)
 
 
+def test_env_terminates(pendulum_on_cart_ocp_swingup_env: PendulumOnCartSwingupEnv):
+    """Test if the environment terminates correctly when applying minimum and maximum control inputs.
+
+    This test ensures that the environment terminates properly when applying either minimum or maximum control
+    inputs continuously. It checks both termination conditions and verifies that the episode ends with a termination
+    rather than a truncation.
+    """
+
+    env = pendulum_on_cart_ocp_swingup_env
+
+    for action in [env.action_space.low, env.action_space.high]:  # type:ignore
+        env.reset(seed=0)
+        for _ in range(1000):
+            state, _, term, trunc, _ = env.step(action)
+            if term:
+                break
+        assert term
+        assert not trunc
+        assert (
+            state[0] < -pendulum_on_cart_ocp_swingup_env.x_threshold
+            or state[0] > pendulum_on_cart_ocp_swingup_env.x_threshold
+        )
+
+
+def test_env_truncates(pendulum_on_cart_ocp_swingup_env: PendulumOnCartSwingupEnv):
+    """Test if the environment truncates correctly when applying minimum and maximum control inputs.
+
+    This test ensures that the environment truncates properly when doing nothing (i.e. it cannot come from termination).
+    It checks both termination conditions and verifies that the episode ends with a truncation
+    rather than a truncation.
+    """
+
+    env = pendulum_on_cart_ocp_swingup_env
+    env.reset(seed=0)
+
+    action = np.array([0])
+    for _ in range(1000):
+        _, _, term, trunc, _ = env.step(action)
+        if trunc:
+            break
+    assert not term
+    assert trunc
+
+
 def test_closed_loop_rendering(
     learnable_pendulum_on_cart_mpc_lls_cost: PendulumOnCartMPC,
-    pendulum_on_cart_ocp_env_lls_cost: PendulumOnCartOcpEnv,
+    pendulum_on_cart_ocp_swingup_env: PendulumOnCartSwingupEnv,
 ):
-    obs, _ = pendulum_on_cart_ocp_env_lls_cost.reset(seed=1337)
+    obs, _ = pendulum_on_cart_ocp_swingup_env.reset(seed=1337)
 
     count = 0
     terminated = False
@@ -79,24 +124,24 @@ def test_closed_loop_rendering(
     cwd = os.getcwd()
     savefile_dir_path = os.path.join(cwd, "test_closed_loop_pendulum_on_cart")
     create_dir_if_not_exists(savefile_dir_path)
-    while count < 200 and not terminated and not truncated:
+    while count < 300 and not terminated and not truncated:
         a = learnable_pendulum_on_cart_mpc_lls_cost.policy(
             obs[0], learnable_pendulum_on_cart_mpc_lls_cost.default_p_global
         )[0]
         obs_prime, r, terminated, truncated, info = (
-            pendulum_on_cart_ocp_env_lls_cost.step(a)
+            pendulum_on_cart_ocp_swingup_env.step(a)
         )
-        frames.append(info["frame"])
+        frames.append(pendulum_on_cart_ocp_swingup_env.render())
         obs = obs_prime
         count += 1
     assert (
-        count < 200
-    ), "max_time and dt dictate that no more than 100 steps should be possible until termination."
+        count <= 200
+    ), "max_time and dt dictate that no more than 200 steps should be possible until termination."
     save_video(
         frames,  # type:ignore
         video_folder=savefile_dir_path,
         name_prefix="pendulum_on_cart",
-        fps=pendulum_on_cart_ocp_env_lls_cost.metadata["render_fps"],
+        fps=pendulum_on_cart_ocp_swingup_env.metadata["render_fps"],
     )
 
     shutil.rmtree(savefile_dir_path)
