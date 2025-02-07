2205c5e555c4435a4aff82652a06f7c5c1d49131
Confectio <leonard.fichtner@gmail.com>
2025-02-07 17:09:29 +0100

    Slight changes in comment

--- leap_c/examples/pendulum_on_a_cart/task.py
+++ leap_c/examples/pendulum_on_a_cart/task.py
@@ -16,20 +16,21 @@ class SwingUpShortHorizonFOU(Task):
 
     def prepare_nn_input(self, obs: Any) -> ndarray:
         # TODO: Where is the tensor conversion currently happening while exploring
-        # (while training it is happening in the buffer already. Should it?)?
+        # (while training it is happening in the buffer already. Should it even, or do we want to
+        # do it here now?)?
         return obs
 
     def prepare_mpc_input(self, obs: Any, param_nn: ndarray | None = None) -> MPCInput:
         x0 = tensor_to_numpy(obs).astype(np.float64)
         # TODO: We probably should add an action to this function in the interface,
         # in case someone wants to use MPC Q functions? Also to prepare_nn_input.
-        # (Or do we want to ignore MPC Q function for the moment?)
+        # (Or do we want to ignore MPC Q functions for the moment?)
 
-        # TODO: I think there needs to be clarification how precisely a Task should be defined.
+        # TODO: I think there needs to be clarification how precisely a Task definition should be.
         # E.g., in prepare_mpc_input I have to define the MPCInput, in particular how the parameters
         # look like. But I don't even have the information about what p_global actually means.
         # Do I have to scale them? To what bounds? Which ones do I need to put into
-        # p_global, p_stagewise, p_W, p_yref? If any?
+        # p_global, p_stagewise, p_W, p_yref (If any)?
         # => I think either the task should define the mpc itself, or we introduce something that
         # keeps track of what the parameters mean, what params are global, etc., in the MPC class (MPC).
         # I prefer the former (I think the latter would be a param manager? :D)
