18ab26e8b1aa17201b93f08af87b10dfb545142e
Confectio <leonard.fichtner@gmail.com>
2025-02-24 10:00:10 +0100

    Add new file for experiments

---
+++ leap_c/scripts/rl/initialization_experiments.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class Experiment(Enum):
+    POINTMASS_FOU_SHIFT = 0
+    POINTMASS_FOU_DEFAULTINIT = 1
+    POINTMASS_FOU_RELOAD = 2
+    POINTMASS_FOU_NN = 3
+
+
+if __name__ == "__main__":
+    pass
