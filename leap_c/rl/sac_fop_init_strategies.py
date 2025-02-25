85f1e74c4706ae1fc44edc1973bb76106b135d46
Confectio <leonard.fichtner@gmail.com>
2025-02-25 13:34:21 +0100

    cleanup for pr

--- leap_c/rl/sac_fop_init_strategies.py
+++
@@ -1,5 +0,0 @@
-from leap_c.rl.sac_fop import SACFOPTrainer
-
-
-class SACFOUTrainerPreviousInit(SACFOPTrainer):
-    """SAC-FO-U but always initializing the MPC with the solution from the previous call."""
