011835227001b32c1a34e3df1f75218958d54434
Confectio <leonard.fichtner@gmail.com>
2025-02-25 15:55:31 +0100

    Average rollout statistics

--- leap_c/rl/sac_fop.py
+++ leap_c/rl/sac_fop.py
@@ -212,12 +212,16 @@ class SACFOPTrainer(Trainer):
         while True:
             if is_terminated or is_truncated:
                 obs, _ = self.train_env.reset()
-                if episode_length < np.inf:
-                    mpc_stats_summed_up_rollout["episode_return"] = episode_return
-                    mpc_stats_summed_up_rollout["episode_length"] = episode_length
-                    self.report_stats(
-                        "train_rollout", mpc_stats_summed_up_rollout, self.state.step
-                    )
+                if (
+                    episode_length < np.inf
+                ):  # TODO: Add rollout-logging for the non-episodic case
+                    stats = {
+                        k: v / episode_length
+                        for k, v in mpc_stats_summed_up_rollout.items()
+                    }
+                    stats["episode_return"] = episode_return
+                    stats["episode_length"] = episode_length
+                    self.report_stats("train_rollout", stats, self.state.step)
                 policy_state = self.init_policy_state()
                 mpc_stats_summed_up_rollout = {}
                 is_terminated = is_truncated = False
