e7380fb2d3b501254ced016f2b9f104737b778a2
Rudolf <reiter11@gmx.at>
2025-03-04 11:18:39 +0100

    minor on quadrotor task

--- leap_c/examples/quadrotor/task.py
+++ leap_c/examples/quadrotor/task.py
@@ -4,8 +4,8 @@ from collections import OrderedDict
 import gymnasium as gym
 import torch
 import numpy as np
-from leap_c.examples.pendulum_on_a_cart.env import PendulumOnCartSwingupEnv
-from leap_c.examples.pendulum_on_a_cart.mpc import PendulumOnCartMPC, PARAMS
+from leap_c.examples.quadrotor.env import QuadrotorStop
+from leap_c.examples.quadrotor.mpc import QuadrotorMPC
 from leap_c.nn.modules import MPCSolutionModule
 from leap_c.registry import register_task
 from leap_c.task import Task
@@ -13,74 +13,22 @@ from leap_c.task import Task
 from ...mpc import MPCInput, MPCParameter
 
 
-PARAMS_SWINGUP = OrderedDict(
+PARAMS_QUADROTOR = OrderedDict(
     [
-        ("M", np.array([1.0])),  # mass of the cart [kg]
-        ("m", np.array([0.1])),  # mass of the ball [kg]
+        ("m", np.array([0.6])),  # mass of the ball [kg]
         ("g", np.array([9.81])),  # gravity constant [m/s^2]
-        ("l", np.array([0.8])),  # length of the rod [m]
-        # The quadratic cost matrix is calculated according to L@L.T
-        ("L11", np.array([np.sqrt(2e3)])),
-        ("L22", np.array([np.sqrt(2e3)])),
-        ("L33", np.array([np.sqrt(1e-2)])),
-        ("L44", np.array([np.sqrt(1e-2)])),
-        ("L55", np.array([np.sqrt(2e-1)])),
-        ("Lloweroffdiag", np.array([0.0] * (4 + 3 + 2 + 1))),
-        (
-            "c1",
-            np.array([0.0]),
-        ),  # position linear cost, only used for non-LS (!) cost
-        (
-            "c2",
-            np.array([0.0]),
-        ),  # theta linear cost, only used for non-LS (!) cost
-        (
-            "c3",
-            np.array([0.0]),
-        ),  # v linear cost, only used for non-LS (!) cost
-        (
-            "c4",
-            np.array([0.0]),
-        ),  # thetadot linear cost, only used for non-LS (!) cost
-        (
-            "c5",
-            np.array([0.0]),
-        ),  # u linear cost, only used for non-LS (!) cost
-        (
-            "xref1",
-            np.array([0.0]),
-        ),  # reference position, only used for LS cost
-        (
-            "xref2",
-            np.array([0.0]),
-        ),  # reference theta, only used for LS cost
-        (
-            "xref3",
-            np.array([0.0]),
-        ),  # reference v, only used for LS cost
-        (
-            "xref4",
-            np.array([0.0]),
-        ),  # reference thetadot, only used for LS cost
-        (
-            "uref",
-            np.array([0.0]),
-        ),  # reference u, only used for LS cost
     ]
 )
 
 
-@register_task("pendulum_swingup")
+@register_task("quadrotor_stop")
 class PendulumOnCart(Task):
-    """Swing-up task for the pendulum on a cart system.
-    The task is to swing up the pendulum from a downward position to the upright position
-    (and balance it there)."""
 
     def __init__(self):
-        params = PARAMS_SWINGUP
-        learnable_params = ["xref2"]
+        params = PARAMS_QUADROTOR
+        learnable_params = ["m"]
 
-        mpc = PendulumOnCartMPC(N_horizon=6, T_horizon=0.25, learnable_params=learnable_params, params=params)
+        mpc = QuadrotorMPC(learnable_params=learnable_params, given_default_param_dict=params)
         mpc_layer = MPCSolutionModule(mpc)
         super().__init__(mpc_layer, PendulumOnCartSwingupEnv)
 
