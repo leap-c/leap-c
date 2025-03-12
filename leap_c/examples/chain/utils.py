0567368ff76a49f983d0a4d752ea40abe9071bd4
dirk <dirk.p.reinhardt@ntnu.no>
2025-03-12 16:52:56 +0100

    Simplify ellipsoid

--- leap_c/examples/chain/utils.py
+++ leap_c/examples/chain/utils.py
@@ -509,15 +509,20 @@ class Ellipsoid:
 
     def __init__(self, center: np.ndarray, radii: np.ndarray, seed: int = 0):
         self.center = center
-        self.covariance = np.diag(1 // radii**2)
-        self.ellipsoid = self.spherical_to_cartesian(phi=np.linspace(0, 2 * np.pi, 100), theta=np.linspace(0, np.pi, 100))
+        self.radii = radii
+
+        self.surface = self.spherical_to_cartesian(phi=np.linspace(0, 2 * np.pi, 100), theta=np.linspace(0, np.pi, 100))
         self.seed = seed
         self.rng = np.random.default_rng(seed)
 
     def plot_surface(self) -> plt.Figure:
         fig = plt.figure(figsize=plt.figaspect(1))
         ax = fig.add_subplot(111, projection="3d")
-        ax.plot_wireframe(*self.ellipsoid.transpose(2, 0, 1), rstride=4, cstride=4, color="b", alpha=0.75)
+        ax.plot_wireframe(*self.surface.transpose(2, 0, 1), rstride=4, cstride=4, color="b", alpha=0.75)
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
+        ax.set_zlabel("z")
+        ax.set_title("Ellipsoid")
         return fig
 
     def plot_points(self, points: np.ndarray) -> plt.Figure:
@@ -527,16 +532,11 @@ class Ellipsoid:
         return fig
 
     def spherical_to_cartesian(self, phi: np.ndarray | float, theta: np.ndarray | float) -> np.ndarray:
-        x = np.outer(np.cos(phi), np.sin(theta))
-        y = np.outer(np.sin(phi), np.sin(theta))
-        z = np.outer(np.ones_like(phi), np.cos(theta))
-        sphere = np.stack((x, y, z), axis=-1)[..., None]
+        x = self.radii[0] * np.outer(np.cos(phi), np.sin(theta)) + self.center[0]
+        y = self.radii[1] * np.outer(np.sin(phi), np.sin(theta)) + self.center[1]
+        z = self.radii[2] * np.outer(np.ones_like(phi), np.cos(theta)) + self.center[2]
 
-        e, v = np.linalg.eig(self.covariance)
-        s = v @ np.diag(np.sqrt(e)) @ v.T
-
-        bias = np.array([10, 0, 0])
-        out = (s @ sphere).squeeze(-1) + bias
+        out = np.stack((x, y, z), axis=-1)
 
         if type(phi) is float:
             return out.squeeze()
