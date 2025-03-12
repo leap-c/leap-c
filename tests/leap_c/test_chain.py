0e2f357fc1d3acbca34bb4be92af3cfea29c82a9
dirk <dirk.p.reinhardt@ntnu.no>
2025-03-12 10:59:41 +0100

    Make chain closed-loop tests actually test something

--- tests/leap_c/test_chain.py
+++ tests/leap_c/test_chain.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from leap_c.examples.chain.mpc import ChainMpc
+
 from leap_c.examples.chain.env import ChainEnv
-from leap_c.examples.chain.utils import animate_chain_position_3D
+from leap_c.examples.chain.mpc import ChainMpc
+from leap_c.examples.chain.utils import (
+    animate_chain_position_3D,
+)
 
 
 def test_chain_policy_evaluation_works():
@@ -12,28 +15,14 @@ def test_chain_policy_evaluation_works():
     x0 = mpc.ocp_solver.acados_ocp.constraints.x0
 
     # Move the second mass a bit in x direction
-    # x0[3] += 0.1
+    x0[3] += 0.1
 
-    # u0, du0_dp_global, status = mpc.policy(state=x0, sens=False, p_global=None)
+    u0, du0_dp_global, status = mpc.policy(state=x0, sens=False, p_global=None)
 
-    sim_x = [x0]
-    for _ in range(100):
-        u0, _, status = mpc.policy(state=sim_x[-1], sens=False, p_global=None)
-        assert status == 0, "Policy evaluation failed"
-        sim_x.append(mpc.ocp_solver.get(1, "x"))
-
-    sim_x = np.array(sim_x)
-
-    animate_chain_position_3D(simX=sim_x, xPosFirstMass=np.zeros(3))
-    plt.show()
+    assert status == 0, "Policy evaluation failed"
 
 
-def test_chain_env():
-    env = ChainEnv(n_mass=3)
-    env.reset()
-
-
-def test_chain_env_mpc_closed_loop():
+def test_chain_env_mpc_closed_loop(plot: bool = False, animate: bool = False):
     learnable_params = ["m", "D", "L", "C", "w"]
     n_mass = 3
 
@@ -67,6 +56,9 @@ def test_chain_env_mpc_closed_loop():
     env = ChainEnv(n_mass=n_mass, fix_point=pos_first_mass)
     env.reset()
 
+    x_ref = env.x_ref
+    u_ref = env.u_ref
+
     sim_x = [env.state]
     sim_u = []
 
@@ -81,11 +73,21 @@ def test_chain_env_mpc_closed_loop():
     sim_x = np.array(sim_x)
     sim_u = np.array(sim_u)
 
-    animate_chain_position_3D(sim_x, pos_first_mass)
-    plt.show()
+    error_norm = np.linalg.norm(x_ref - sim_x, axis=1)
+
+    assert error_norm[-1] < 1e-2, "Error norm is too high"
+
+    if plot:
+        plt.figure()
+        plt.plot(error_norm, label="x_ref - x")
+        plt.show()
+
+    if animate:
+        animate_chain_position_3D(sim_x, pos_first_mass)
+
+    # plt.show()
 
 
 if __name__ == "__main__":
     # test_chain_policy_evaluation_works()
-    # test_chain_env()
     test_chain_env_mpc_closed_loop()
