c995d116cdd38b4622a8e71dbe1a107a17fdcf6c
Confectio <leonard.fichtner@gmail.com>
2025-03-18 14:44:14 +0100

    Fix seeding

--- leap_c/examples/chain/env.py
+++ leap_c/examples/chain/env.py
@@ -19,9 +19,9 @@ def _cont_f_expl(
     p: dict[str, np.ndarray],
     fix_point: np.ndarray | None = None,
 ) -> np.ndarray:
-    assert all(key in p for key in ["D", "L", "C", "m", "w"]), (
-        "Not all necessary parameters are in p."
-    )
+    assert all(
+        key in p for key in ["D", "L", "C", "m", "w"]
+    ), "Not all necessary parameters are in p."
 
     if fix_point is None:
         fix_point = np.zeros(3, 1)
@@ -158,7 +158,6 @@ class ChainEnv(gym.Env):
         pos_last_ref: np.ndarray | None = None,
         phi_range: list[tuple[float, float]] = (np.pi / 6, np.pi / 3),
         theta_range: tuple[float, float] = (-np.pi / 4, np.pi / 4),
-        seed: int | None = None,
     ):
         super().__init__()
 
@@ -210,7 +209,7 @@ class ChainEnv(gym.Env):
         self.phi_range = phi_range
         self.theta_range = theta_range
 
-        self.rng = np.random.default_rng(seed)
+        self.rng = None
 
         # self.ellipsoid_center = self.fix_point
         # self.ellispoid_variability_matrix = np.diag()
@@ -253,7 +252,13 @@ class ChainEnv(gym.Env):
     def reset(
         self, *, seed: int | None = None, options: dict | None = None
     ) -> tuple[Any, dict]:  # type: ignore
-        self._np_random = np.random.RandomState(seed)
+        if seed is not None:
+            super().reset(seed=seed)
+            self.observation_space.seed(seed)
+            self.action_space.seed(seed)
+            self.rng = np.random.default_rng(seed)
+        if self._np_random is None:
+            raise RuntimeError("The first reset needs to be called with a seed.")
         self.state_trajectory = None
         self.state, self.action = self._init_state_and_action()
         self.time = 0.0
