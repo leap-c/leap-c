6ca6bec35633ebf317b091ceaacab605e15ec54d
Josip Kir Hromatko <josipkh@gmail.com>
2025-03-31 16:24:53 +0200

    try again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,8 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(".."))
+# sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath('../..'))
 
 project = "leap-c"
 copyright = "2024, Dirk, Jasper, Leonard"
