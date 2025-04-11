d38f52b4c0fc9037247732e12a459d7a6674c3c3
Jasper Hoffmann <hoffmaja@informatik.uni-freiburg.de>
2025-04-11 14:52:52 +0200

    add tests

--- leap_c/nn/utils.py
+++ leap_c/nn/utils.py
@@ -53,5 +53,7 @@ def min_max_scaling(
     # check if low and high are correctly set
     if not (low < high).all():
         raise ValueError("The low bound must be less than the high bound.")
+    if torch.isinf(low).any() or torch.isinf(high).any():
+        raise ValueError("The low and high bounds must not be infinite.")
 
     return (x - low) / (high - low)
