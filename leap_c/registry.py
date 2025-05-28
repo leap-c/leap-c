73b1f8e87655c974adfe6b3c2de41ff0fa7ee7f5
Mazen Amria <t.mazenamria@gmail.com>
2025-05-28 02:14:34 +0200

    removing cfg merge

--- leap_c/registry.py
+++ leap_c/registry.py
@@ -52,9 +52,5 @@ def create_trainer(
 ) -> Trainer:
     if name not in TRAINER_REGISTRY:
         raise ValueError(f"Unknown trainer: {name}")
-    _cfg = DEFAULT_CFG_REGISTRY[name]
-    cfg_dict = {**_cfg.__dict__}
-    if cfg is not None:
-        cfg_dict.update(cfg.__dict__)
-    cfg = type(_cfg)(**cfg_dict)
+    # TODO: implement proper config merging, probably using pydantic
     return TRAINER_REGISTRY[name](task, output_path, device, cfg)  # type: ignore
