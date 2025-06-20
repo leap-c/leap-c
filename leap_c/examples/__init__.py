7e84ddc53518adbc3af2c2f66294600b31314825
Mazen Amria <t.mazenamria@gmail.com>
2025-06-20 18:04:10 +0200

    updating sac fop

--- leap_c/examples/__init__.py
+++ leap_c/examples/__init__.py
@@ -1,5 +1,5 @@
-import leap_c.examples.mujoco.task
-import leap_c.examples.pointmass.task
-import leap_c.examples.cartpole.task
-import leap_c.examples.chain.task
-import leap_c.examples.mujoco.reacher.task
+# import leap_c.examples.mujoco.task
+# import leap_c.examples.pointmass.task
+# import leap_c.examples.cartpole.task
+# import leap_c.examples.chain.task
+# import leap_c.examples.mujoco.reacher.task
