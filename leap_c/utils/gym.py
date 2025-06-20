f52082801217c22c7c4af117fe3940d83bd15528
Mazen Amria <t.mazenamria@gmail.com>
2025-06-20 14:23:09 +0200

    running sac on HalfCheetah-v4

--- leap_c/utils/gym.py
+++ leap_c/utils/gym.py
@@ -2,7 +2,6 @@ import gymnasium as gym
 from gymnasium.wrappers import OrderEnforcing, RecordEpisodeStatistics
 
 
-
 def wrap_train_env(env: gym.Env, seed: int = 0) -> gym.Env:
     """Wraps a gymnasium environment for training.
 
@@ -32,6 +31,8 @@ def wrap_eval_env(env: gym.Env, seed: int = 1) -> gym.Env:
     Returns:
         gym.Env: The wrapped environment for evaluation.
     """
+    # TODO (Mazen): Why not adding RecordEpisodeStatistics here and use it to get the episodic reward in the validation
+    #  instead of accumulating the scores as we're doing?
     env = OrderEnforcing(env)
     env.reset(seed=seed)
     env.observation_space.seed(seed)
