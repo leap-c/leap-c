dd7b2058109953764ff85a830cc0aa7d4224ff02
Confectio <leonard.fichtner@gmail.com>
2025-07-31 14:57:51 +0200

    Added stagewise gradtests

--- leap_c/ocp/acados/diff_mpc.py
+++ leap_c/ocp/acados/diff_mpc.py
@@ -208,6 +208,7 @@ class AcadosDiffMpcFunction(DiffFunction):
                 return None
 
             # TODO (Jasper): Filter out stages that are not needed.
+            # TODO: Remove HVAC Hack / use x_seed_with_stage as defined below
             x_seed_with_stage = (
                 [
                     (stage_idx + 1, x_seed[:, stage_idx + 1][..., None]) #TODO: Remove HVAC Hack
@@ -216,6 +217,15 @@ class AcadosDiffMpcFunction(DiffFunction):
                 if x_seed is not None and not dx_zero
                 else []
             )
+            # This would be correct in general
+            # x_seed_with_stage = (
+            #     [
+            #         (stage_idx, x_seed[:, stage_idx][..., None])
+            #         for stage_idx in range(0, self.ocp.dims.N+1)  # type: ignore
+            #     ]
+            #     if x_seed is not None and not dx_zero
+            #     else []
+            # )
 
             u_seed_with_stage = (
                 [
