879f51c24d83a1b01359b3434257bc02d51c8816
Confectio <leonard.fichtner@gmail.com>
2025-09-23 13:32:15 +0200

    Use AcadosController for examples

--- leap_c/examples/chain/controller.py
+++ leap_c/examples/chain/controller.py
@@ -1,12 +1,8 @@
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Any, Callable
 
-import gymnasium as gym
 import numpy as np
-from acados_template.acados_ocp import AcadosOcp
 
-from leap_c.controller import ParameterizedController
 from leap_c.examples.chain.acados_ocp import (
     ChainAcadosParamInterface,
     ChainInitializer,
@@ -15,7 +11,7 @@ from leap_c.examples.chain.acados_ocp import (
 )
 from leap_c.examples.chain.dynamics import define_f_expl_expr
 from leap_c.examples.chain.utils.resting_chain_solver import RestingChainSolver
-from leap_c.ocp.acados.diff_mpc import AcadosDiffMpcCtx, collate_acados_diff_mpc_ctx
+from leap_c.ocp.acados.controller import AcadosController
 from leap_c.ocp.acados.parameters import AcadosParameter, AcadosParameterManager
 from leap_c.ocp.acados.torch import AcadosDiffMpc
 
@@ -41,7 +37,7 @@ class ChainControllerConfig:
     param_interface: ChainAcadosParamInterface = "global"
 
 
-class ChainController(ParameterizedController):
+class ChainController(AcadosController):
     """Acados-based controller for the hanging chain system.
     The state and action correspond to the observation and action of the Chain environment.
     The cost function takes the form of a weighted least-squares cost on the full state and action
@@ -52,18 +48,9 @@ class ChainController(ParameterizedController):
     Attributes:
         cfg: A configuration object containing high-level settings for the MPC problem,
             such as horizon length.
-        ocp: The acados ocp object representing the optimal control problem structure.
-        param_manager: For managing the parameters of the ocp.
-        diff_mpc: An object wrapping the acados ocp solver for differentiable MPC solving.
-        collate_fn_map: A mapping for collating AcadosDiffMpcCtx objects in batches.
     """
 
     cfg: ChainControllerConfig
-    ocp: AcadosOcp
-    param_manager: AcadosParameterManager
-    diff_mpc: AcadosDiffMpc
-
-    collate_fn_map: dict[type, Callable] = {AcadosDiffMpcCtx: collate_acados_diff_mpc_ctx}
 
     def __init__(
         self,
@@ -132,20 +119,3 @@ class ChainController(ParameterizedController):
             initializer=initializer,
             export_directory=export_directory,
         )
-
-    def forward(self, obs, param, ctx=None) -> tuple[Any, np.ndarray]:
-        p_stagewise = self.param_manager.combine_non_learnable_parameter_values(
-            batch_size=obs.shape[0]
-        )
-        ctx, u0, x, u, value = self.diff_mpc(obs, p_global=param, p_stagewise=p_stagewise, ctx=ctx)
-        return ctx, u0
-
-    def jacobian_action_param(self, ctx) -> np.ndarray:
-        return self.diff_mpc.sensitivity(ctx, field_name="du0_dp_global")
-
-    @property
-    def param_space(self) -> gym.Space:
-        return self.param_manager.get_param_space(dtype=np.float32)
-
-    def default_param(self, obs) -> np.ndarray:
-        return self.param_manager.learnable_parameters_default.cat.full().flatten()  # type:ignore
--- leap_c/examples/pointmass/controller.py
+++ leap_c/examples/pointmass/controller.py
@@ -2,17 +2,14 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Any
 
-import gymnasium as gym
 import numpy as np
-from acados_template.acados_ocp import AcadosOcp
 
-from leap_c.controller import ParameterizedController
 from leap_c.examples.pointmass.acados_ocp import (
     PointMassAcadosParamInterface,
     create_pointmass_params,
     export_parametric_ocp,
 )
-from leap_c.ocp.acados.diff_mpc import AcadosDiffMpcCtx, collate_acados_diff_mpc_ctx
+from leap_c.ocp.acados.controller import AcadosController
 from leap_c.ocp.acados.parameters import AcadosParameter, AcadosParameterManager
 from leap_c.ocp.acados.torch import AcadosDiffMpc
 
@@ -34,7 +31,7 @@ class PointMassControllerConfig:
     param_interface: PointMassAcadosParamInterface = "global"
 
 
-class PointMassController(ParameterizedController):
+class PointMassController(AcadosController):
     """Acados-based controller for the PointMass system.
     The state corresponds to the observation of the PointMass environment, without the wind force.
     The cost function takes a weighted least-squares form,
@@ -45,18 +42,9 @@ class PointMassController(ParameterizedController):
     Attributes:
         cfg: A configuration object containing high-level settings for the MPC problem,
             such as horizon length.
-        ocp: The acados ocp object representing the optimal control problem structure.
-        param_manager: For managing the parameters of the ocp.
-        diff_mpc: An object wrapping the acados ocp solver for differentiable MPC solving.
-        collate_fn_map: A mapping for collating AcadosDiffMpcCtx objects in batches.
     """
 
     cfg: PointMassControllerConfig
-    ocp: AcadosOcp
-    param_manager: AcadosParameterManager
-    diff_mpc: AcadosDiffMpc
-
-    collate_fn_map = {AcadosDiffMpcCtx: collate_acados_diff_mpc_ctx}
 
     def __init__(
         self,
@@ -107,13 +95,3 @@ class PointMassController(ParameterizedController):
         x0 = obs[:, :4]
         ctx, u0, x, u, value = self.diff_mpc(x0, p_global=param, p_stagewise=p_stagewise, ctx=ctx)
         return ctx, u0
-
-    def jacobian_action_param(self, ctx) -> np.ndarray:
-        return self.diff_mpc.sensitivity(ctx, field_name="du0_dp_global")
-
-    @property
-    def param_space(self) -> gym.Space:
-        return self.param_manager.get_param_space(dtype=np.float32)
-
-    def default_param(self, obs) -> np.ndarray:
-        return self.param_manager.learnable_parameters_default.cat.full().flatten()  # type:ignore
