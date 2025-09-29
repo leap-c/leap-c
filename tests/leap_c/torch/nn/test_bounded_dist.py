9aa20ed52db876c5b527d109cf0a482aa6b3da4d
Confectio <leonard.fichtner@gmail.com>
2025-09-29 16:42:36 +0200

    Test more extreme values

--- tests/leap_c/torch/nn/test_bounded_dist.py
+++ tests/leap_c/torch/nn/test_bounded_dist.py
@@ -8,13 +8,15 @@ from leap_c.torch.nn.bounded_distributions import ScaledBeta
 def test_scaled_beta():
     """Sanity checks for the ScaledBeta distribution."""
 
-    test_space = spaces.Box(low=np.array([-10.0, -15.0, 31.0]), high=np.array([-5.0, 20.0, 42.0]))
+    test_space = spaces.Box(
+        low=np.array([-10.0, -15.0, 31.0, 3.0]), high=np.array([-5.0, 20.0, 42.0, 4.0])
+    )
     dist = ScaledBeta(test_space)
 
     # Define parameters
     def create_alpha_beta_tensors():
-        alpha = torch.tensor([[1.0, -2.0, -3.0], [3.0, 4.0, -5.0]])
-        beta = torch.tensor([[4.0, 3.0, 2.0], [2.0, -1.0, 0.0]])
+        alpha = torch.tensor([[1.0, -2.0, -3.0, -100.0], [3.0, 4.0, -5.0, 100.0]])
+        beta = torch.tensor([[4.0, 3.0, 2.0, -100.0], [2.0, -1.0, 0.0, 100.0]])
         alpha.requires_grad = True
         beta.requires_grad = True
         return alpha, beta
@@ -23,7 +25,7 @@ def test_scaled_beta():
     samples, log_prob, _ = dist(alpha, beta, deterministic=False)
 
     # Check shapes
-    assert samples.shape == (2, 3)
+    assert samples.shape == (2, 4)
     assert log_prob.shape == (2, 1)
 
     # Check that samples are within bounds
@@ -48,7 +50,7 @@ def test_scaled_beta():
     mode_samples, mode_log_prob, _ = dist(alpha, beta, deterministic=True)
 
     # Check that mode samples are within bounds and their shapes
-    assert mode_samples.shape == (2, 3)
+    assert mode_samples.shape == (2, 4)
     assert mode_log_prob.shape == (2, 1)
     mode_samples_npy = mode_samples.detach().numpy()
     for s in mode_samples_npy:
