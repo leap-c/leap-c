3187299899b053fd3a85eba6a5c8c0d7cf73135e
dirk <dirk.p.reinhardt@ntnu.no>
2025-11-20 13:54:24 +0100

    Only subset of controllers in testscript

--- tests/leap_c/test_scripts.py
+++ tests/leap_c/test_scripts.py
@@ -132,10 +132,10 @@ def test_find_all_scripts(script, scripts_dict):
 
 def _run_script(script, tmp_path, env, reuse_code_dir, controller=None):
     if controller in [
-        # "cartpole",
-        # "cartpole_stagewise",
-        # "chain",
-        # "chain_stagewise",
+        "cartpole",
+        "cartpole_stagewise",
+        "chain",
+        "chain_stagewise",
         # "pointmass",
         # "pointmass_stagewise",
         # "hvac",
