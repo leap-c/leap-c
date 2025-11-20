fb11b9b56c0ef67d0ac028baf6947d26f7d96239
dirk <dirk.p.reinhardt@ntnu.no>
2025-11-20 14:52:51 +0100

    Include cartpole

--- tests/leap_c/test_scripts.py
+++ tests/leap_c/test_scripts.py
@@ -132,7 +132,7 @@ def test_find_all_scripts(script, scripts_dict):
 
 def _run_script(script, tmp_path, env, reuse_code_dir, controller=None):
     if controller in [
-        "cartpole",
+        # "cartpole",
         "cartpole_stagewise",
         "chain",
         "chain_stagewise",
