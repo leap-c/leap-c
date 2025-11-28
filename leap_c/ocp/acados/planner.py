a13aeecbd6d69ef9274a2733c34594223a1803df
JasperHoffmann <41264755+JasperHoffmann@users.noreply.github.com>
2025-11-28 13:06:23 +0100

    Combine default learnable param (#189)
    This PR provides a new method
    `combine_default_learnable_parameter_values` for the
    `AcadosParameterManager` to combine default values with overwrites to
    build a default for the learnable parameter:
    
    - removed custom hvac combine logic in the `default_param` method
    - 10x speedup by avoiding for loops
    - general `combine_default_learnable_parameter_values` method

--- leap_c/ocp/acados/planner.py
+++ leap_c/ocp/acados/planner.py
@@ -1,6 +1,7 @@
 from typing import Generic, get_args
 
 import gymnasium as gym
+import numpy as np
 import torch
 from numpy import ndarray
 
@@ -73,5 +74,9 @@ class AcadosPlanner(ParameterizedPlanner[CtxType], Generic[CtxType]):
     def param_space(self) -> gym.Space:
         return self.param_manager.get_param_space()
 
-    def default_param(self, obs: ndarray | None) -> ndarray:
-        return self.param_manager.learnable_parameters_default.cat.full().flatten()  # type:ignore
+    def default_param(self, obs: ndarray) -> ndarray:
+        default = self.param_manager.learnable_parameters_default.cat.full().flatten()  # type:ignore
+        if obs.ndim <= 1:
+            return default
+        batch_size = obs.shape[0]
+        return np.tile(default, (batch_size, 1)) if batch_size > 1 else default[None, :]
