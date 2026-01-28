3679cb4569701c9eae12cd55b0cfd096e93ca739
Filippo Airaldi <f.airaldi@tudelft.nl>
2026-01-28 12:20:41 +0100

    added tests for ModeConcentrationBeta

--- tests/leap_c/torch/nn/test_bounded_dist.py
+++ tests/leap_c/torch/nn/test_bounded_dist.py
@@ -2,7 +2,11 @@ import numpy as np
 import torch
 from gymnasium.spaces import Box
 
-from leap_c.torch.nn.bounded_distributions import ScaledBeta, SquashedGaussian
+from leap_c.torch.nn.bounded_distributions import (
+    ModeConcentrationBeta,
+    ScaledBeta,
+    SquashedGaussian,
+)
 
 
 def test_scaled_beta():
@@ -154,3 +158,102 @@ def test_squashed_gaussian_anchor():
 #     assert samples_stochastic.shape == (1, 2)
 #     assert torch.all(samples_stochastic >= torch.from_numpy(test_space.low))
 #     assert torch.all(samples_stochastic <= torch.from_numpy(test_space.high))
+
+
+def test_mode_concentration_beta():
+    """Sanity checks for the `ModeConcentrationBeta` distribution."""
+    rng = np.random.default_rng()
+
+    # generate random space and associated distribution
+    ndim, n_samples = map(int, rng.integers(2, 10, size=2))
+    low = -5 - np.abs(rng.normal(scale=5, size=ndim))
+    high = 5 + np.abs(rng.normal(scale=5, size=ndim))
+    space = Box(low, high, dtype=np.float64)
+    distribution = ModeConcentrationBeta(space)
+
+    def _create_random_params() -> tuple[torch.Tensor, torch.Tensor]:
+        logit_mode = torch.from_numpy(rng.normal(size=(n_samples, ndim)))
+        logit_log_conc = torch.from_numpy(rng.normal(size=(n_samples, ndim)))
+        return logit_mode.requires_grad_(), logit_log_conc.requires_grad_()
+
+    # check samples lie in the space
+    samples: torch.Tensor
+    log_prob: torch.Tensor
+    logit_mode, logit_log_conc = _create_random_params()
+    samples, log_prob, _ = distribution(logit_mode, logit_log_conc, deterministic=False)
+    assert all(s in space for s in samples.numpy(force=True))
+    assert log_prob.shape == (n_samples, 1)
+
+    # test backward of samples and  log_prob works
+    samples.sum().backward(retain_graph=True)
+    for t in (logit_mode, logit_log_conc):
+        assert t.grad is not None and not t.grad.isnan().any().item()
+        t.grad = None  # reset for next test
+    log_prob.sum().backward()
+    for t in (logit_mode, logit_log_conc):
+        assert t.grad is not None and not t.grad.isnan().any().item()
+
+    # check that deterministic samples (modes themselves) lie in the space and are equal to the
+    # requested modes
+    logit_mode, logit_log_conc = _create_random_params()
+    samples, log_prob, _ = distribution(logit_mode, logit_log_conc, deterministic=True)
+    assert all(s in space for s in samples.numpy(force=True))
+    assert samples.shape == (n_samples, ndim)
+    with torch.no_grad():
+        mode_01 = distribution.padding + (1.0 - 2.0 * distribution.padding) * torch.sigmoid(
+            logit_mode
+        )
+        expected_samples = distribution.lb + distribution.scale * mode_01
+        torch.testing.assert_close(samples, expected_samples)
+
+    # test mode backward works but does not include log_conc, and that log_prob doesn't require grad
+    samples.sum().backward()
+    assert logit_mode.grad is not None and not logit_mode.grad.isnan().any().item()
+    assert logit_log_conc.grad is None
+    assert not log_prob.requires_grad
+
+
+def test_mode_concentration_beta_anchor():
+    """Test anchor functionality for `ModeConcentrationBeta` distribution."""
+    rng = np.random.default_rng()
+
+    # generate random space and associated distribution
+    ndim, n_samples = map(int, rng.integers(2, 10, size=2))
+    low = -5 - np.abs(rng.normal(scale=5, size=ndim))
+    high = 5 + np.abs(rng.normal(scale=5, size=ndim))
+    space = Box(low, high, dtype=np.float64)
+    distribution = ModeConcentrationBeta(space, padding=0)  # remove paddings to avoid distorsion
+
+    # test deterministic sampling with anchor - when logit_mode=0, mode is on anchor
+    logit_mode = torch.zeros((n_samples, ndim), dtype=torch.float64, requires_grad=True)
+    logit_log_conc = torch.from_numpy(rng.normal(size=(n_samples, ndim))).requires_grad_()
+    anchor = torch.from_numpy(rng.uniform(low, high, size=(n_samples, ndim)))
+    samples: torch.Tensor
+    log_prob: torch.Tensor
+    samples, log_prob, _ = distribution(logit_mode, logit_log_conc, True, anchor)
+    assert all(s in space for s in samples.numpy(force=True))
+    assert log_prob.shape == (n_samples, 1)
+    torch.testing.assert_close(samples, anchor)
+
+    # test gradients work with anchor
+    samples.sum().backward()
+    assert logit_mode.grad is not None and not logit_mode.grad.isnan().any().item()
+    assert logit_log_conc.grad is None
+    assert not log_prob.requires_grad
+
+    # test stochastic sampling with anchor
+    logit_mode = torch.from_numpy(rng.normal(size=(n_samples, ndim))).requires_grad_()
+    logit_log_conc = torch.from_numpy(rng.normal(size=(n_samples, ndim))).requires_grad_()
+    anchor = torch.from_numpy(rng.uniform(low, high, size=(n_samples, ndim)))
+    samples, log_prob, _ = distribution(logit_mode, logit_log_conc, anchor=anchor)
+    assert all(s in space for s in samples.numpy(force=True))
+    assert log_prob.shape == (n_samples, 1)
+
+    # test gradients work with anchor in stochastic mode
+    samples.sum().backward(retain_graph=True)
+    for t in (logit_mode, logit_log_conc):
+        assert t.grad is not None and not t.grad.isnan().any().item()
+        t.grad = None  # reset for next test
+    log_prob.sum().backward()
+    for t in (logit_mode, logit_log_conc):
+        assert t.grad is not None and not t.grad.isnan().any().item()
